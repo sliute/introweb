45a6cec095a3ab5df38b8e044f9ae8731f94b849
Enrico Graziani <mrenrich84@gmail.com>
2017-01-16 16:53:48 +0000

    Updated named_cat to use post, created naming

--- app.rb
+++ app.rb
@@ -17,8 +17,12 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
-  erb(:index)
+  erb(:named_cat)
+end
+
+get '/naming' do
+  erb(:naming)
 end
