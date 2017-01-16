3bfefbdcee5417bb94e45b91bae61d1c9a8c5049
Stefan Liute <stefanliute@gmail.com>
2017-01-16 15:57:49 +0000

    Sinatra: /cat

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+get '/' do
+  'hello!'
+end
+
+get '/secret' do
+  'Mango icecream for all!'
+end
+
+get '/shotgun' do
+  'Shotgun is running as advertised'
+end
+
+get '/cat' do
+  erb :cat
+end
